1f0c4e1a6be37e1f5dc37c398c88d0e54dea28dd
Jabberwockiii <jacob4782241@gmail.com>
2022-05-30 10:49:42 -0400

    mergeTwoList

--- parathenisis.py
+++ parathenisis.py
@@ -1,17 +1,32 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution(object):
-    def isValid(self, s):
+    def mergeTwoLists(self, list1, list2):
         """
-        :type s: str
-        :rtype: bool
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
         """
-        pairs = {"{": "}", "[": "]", "(": ")"}
-        stack = []
-        for i in s:
-            if i in pairs:
-                stack.append(i)
-            elif not stack or pairs[stack.pop()] != i:
-                return False
-        return not stack
+        head = list1
+        while(list1.next != None or list2.next != None):
+            if(list1.next == None):
+                list1.next = list2
+                break
+            elif(list2.next == None):
+                list2.next = list1
+                break
+            elif(list1.val < list2.val):
+                list1.next = list2
+                list2 = list2.next
+            elif(list1.val >= list2.val):
+                prev = list1
+                list2.next = list1
+                list1 = prev
+        return head 
         
-
+                
+            
         
