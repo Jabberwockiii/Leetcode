f246cb5bb1d64950780fe940710855630d830371
Jabberwockiii <jacob4782241@gmail.com>
2022-06-17 14:02:55 -0400

    an alternative

---
+++ 41.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        #answer range[1,n+1]
+        #exclude negative number and number bigger than n+1
+        for i in range(n):
+            if nums[i] < 0 or nums[i] > n:
+                nums[i] = 0
+        nums.sort()
+        for i in range(n):
+            if(nums[i] == 0):
+                continue
+            if(nums[i] == nums[i-1] + 1):
+                continue
+            else:
+                return nums[i]
+        return nums[n] + 1 
--- combinationSum.py
+++ combinationSum.py
@@ -1,17 +1,15 @@
 class Solution(object):
-    def combinationSum(self, candidates, target):
+    def combinationSum2(self, candidates, target):
         ret = []
-        self.dfs(candidates, target, [], ret)
+        self.dfs(sorted(candidates), target, 0, [], ret)
         return ret
     
-    def dfs(self, nums, target, path, ret):
-        if target < 0:
+    def dfs(self, nums, target, idx, path, ret):
+        if target <= 0:
+            if target == 0:
+                ret.append(path)
             return 
-        if target == 0:
-            ret.append(path)
-            return 
-        for i in range(len(nums)):
-            self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret)
-            
+        for i in range(idx, len(nums)):
+            self.dfs(nums, target-nums[i], i+1, path+[nums[i]], ret)
 s = Solution()
-print(s.combinationSum([2,3,6,7], 7))
+print(s.combinationSum2([10,1,2,7,6,1,5], 8))
